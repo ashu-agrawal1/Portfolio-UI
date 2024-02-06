47177f847ac7677afa0e477348d01b30cd9081ed
unknown <ashuagrawal3000@gmail.com>
2024-02-06 18:47:56 +0530

    animation issue fixed

--- animations.js
+++ animations.js
@@ -1,13 +1,30 @@
-const observer = new IntersectionObserver((entries)=>{
-    entries.forEach(entry=>{
-        if(entry.isIntersecting){
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
             entry.target.classList.add("show");
         }
-        else{
+        else {
             entry.target.classList.remove("show")
         }
     })
 })
 
-const hiddenElemnts = document.querySelectorAll(".hidden")
-hiddenElemnts.forEach(el=>observer.observe(el))
+const hiddenElemnts = document.querySelectorAll(".hidden2")
+hiddenElemnts.forEach(el => observer.observe(el))
+
+
+const hiddenElemnts2 = document.querySelectorAll(".hidden")
+window.addEventListener("scroll", animate)
+
+function animate() {
+    let windowHeight = window.innerHeight;
+    let revealPoint = 150;
+    hiddenElemnts2.forEach(el => {
+        if (el.getBoundingClientRect().top < windowHeight - revealPoint) {
+            el.classList.add("show")
+        }
+        else {
+            el.classList.remove("show")
+        }
+    });
+}
